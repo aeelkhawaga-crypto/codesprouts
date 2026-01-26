53ddf12035b20bf33ec22692e1432d265a81c55a
aeelkhawaga-crypto <234664765+aeelkhawaga-crypto@users.noreply.github.com>
2026-01-26 18:46:05 -0500

    Updates

--- _next/static/Dm-1-Yp71EFKNLEh8aR-c/_buildManifest.js
+++ _next/static/Dm-1-Yp71EFKNLEh8aR-c/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/codesprouts//_next/:path+",
+        "destination": "/codesprouts/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
