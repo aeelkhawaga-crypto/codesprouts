a53594daf1ee97a6174296047ee2317526a15277
aeelkhawaga-crypto <234664765+aeelkhawaga-crypto@users.noreply.github.com>
2026-01-26 19:59:36 -0500

    Updates

--- _next/static/CVw_o5lNSIKAjo8g1g6Vn/_buildManifest.js
+++ _next/static/CVw_o5lNSIKAjo8g1g6Vn/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/codesprouts//_next/:path+",
-        "destination": "/codesprouts/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
